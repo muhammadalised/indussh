38a84a078d9a7bd306eee7f6cba5d4b4adea3266
muhammadalised <muhammadalised@gmail.com>
2020-08-20 00:28:33 +0500

    added more files

--- indussh/__init__.py
+++ indussh/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt =  Bcrypt()
 
 def create_app():
 
---
+++ indussh/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = "You shall not guess"
+                              # mysql://username:password@server/db
+    SQLALCHEMY_DATABASE_URI = "mysql:///indussh:indussh@localhost/indus"
