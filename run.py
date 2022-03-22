09125a55df0d18f4c344c858560c5b301bcafaaf
Muhammad Ali <muhammadalised@gmail.com>
2022-03-22 21:24:02 +0500

    added models to shell context

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-from indussh import create_app
+from indussh import create_app, db
+from indussh.models import User, Customer, Product, Order, OrderItem
 
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Customer=Customer, Product=Product, Order=Order, OrderItem=OrderItem)
