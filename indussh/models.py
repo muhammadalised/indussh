088ca200a20fa1b19ab6706275db44f488b500bf
Muhammad Ali <muhammadalised@gmail.com>
2022-06-15 00:09:13 +0500

    Added some methods to User models

--- indussh/models.py
+++ indussh/models.py
@@ -1,8 +1,11 @@
 from indussh import db, login_manager
+
 from flask import current_app
 from flask_login import UserMixin
 from datetime import datetime
 import pandas as pd
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # class Role(db.Model):
 #     __tablename__ = 'roles'
@@ -19,8 +22,25 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), unique=True, nullable=False)
     image_file = db.Column(db.String(20), default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute!')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(self.password_hash, password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
