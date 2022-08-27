0e626d97c7c0443454e165304ed015c0a3f9b612
Muhammad Ali <muhammadalised@gmail.com>
2022-08-27 23:07:51 +0500

    Created class for chatbot

--- indussh/config.py
+++ indussh/config.py
@@ -1,8 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SESSION_TYPE=os.environ.get('SESSION_TYPE')
+    SESSION_TYPE = os.environ.get('SESSION_TYPE')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -13,9 +14,14 @@ class Config:
     PROFILE_IMAGE_SIZE = (125, 125)
     PRODUCT_IMAGE_SIZE = (480, 340)
 
+    CHATBOT_MODEL_PATH = ''
+    CHATBOT_TRAINING_DATA = ''
+    CHATBOT_INTENTS = ''
+
     @staticmethod
     def init_app(app):
         pass
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -23,11 +29,13 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
+
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    
+
+
 config = {
     'development':  DevelopmentConfig,
     'production': ProductionConfig,
---
+++ indussh/products/chatbot.py
@@ -0,0 +1,81 @@
+import pickle
+import json
+import random
+
+import tensorflow as tf
+import numpy as np
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+
+class Chatbot:
+    ERROR_THRESHOLD = 0.65
+
+    def __init__(self, model, intents, training_data):
+        self.stemmer = LancasterStemmer()
+        self.intents = intents
+        self.model = model
+
+        self.words = training_data['words']
+        self.documents = training_data['documents']
+        self.classes = training_data['classes']
+
+    def process_sentence(self, sentence):
+        # Tokenize the pattern
+        sentence_words = nltk.word_tokenize(sentence)
+        # Stem each word
+        sentence_words = [self.stemmer.stem(
+            word.lower()) for word in sentence_words]
+        return sentence_words
+
+    def bag_of_words(self, sentence, words, show_details=False):
+        # Tokenize the pattern
+        sentence_words = self.process_sentence(sentence)
+        # Bag of words
+        bag = [0]*len(words)
+        for s in sentence_words:
+            for i, w in enumerate(words):
+                if w == s:
+                    bag[i] = 1
+                    if show_details:
+                        print("found in bag: %s" % w)
+        return (np.array(bag))
+
+    def classify(self, sentence):
+        # generate probabilities from the model
+        p = self.bag_of_words(sentence, self.words)
+
+        d = len(p)
+        f = len(self.documents)-2
+        a = np.zeros([f, d])
+        tot = np.vstack((p, a))
+
+        results = self.model.predict(tot)[0]
+
+        # filter out predictions below a threshold
+        results = [[i, r]
+                   for i, r in enumerate(results) if r > self.ERROR_THRESHOLD]
+        # sort by strength of probability
+        results.sort(key=lambda x: x[1], reverse=True)
+        return_list = []
+
+        for r in results:
+            return_list.append((self.classes[r[0]], r[1]))
+
+        # return tuple of intent and probability
+        return return_list
+
+    def response(self):
+        pass
+
+    def extract_amount(self, sentence):
+        pass
+
+    def track_user_offers(self, sentence):
+        pass
+
+    def track_bot_offers(self, sentence):
+        pass
+
+    def create_offer(self, min_price, original_price, bot_offer_count):
+        pass
