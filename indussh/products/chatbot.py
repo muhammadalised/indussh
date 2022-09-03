eb61fd54b10e9bebd67d9453cd7c5e15a06bdbf4
muhammadalised <muhammadalised@gmail.com>
2022-09-03 23:25:36 +0500

    Implemented response, generate_offer and compute_offer methods

--- indussh/products/chatbot.py
+++ indussh/products/chatbot.py
@@ -1,5 +1,3 @@
-import pickle
-import json
 import random
 import re
 
@@ -10,7 +8,7 @@ from nltk.stem.lancaster import LancasterStemmer
 
 
 class Chatbot:
-    ERROR_THRESHOLD = 0.65
+    ERROR_THRESHOLD = 0.50
 
     def __init__(self, model, intents, training_data):
         self.stemmer = LancasterStemmer()
@@ -20,6 +18,10 @@ class Chatbot:
         self.words = training_data['words']
         self.documents = training_data['documents']
         self.classes = training_data['classes']
+
+        self.context = {}
+        self.__bot_offers = []
+        self.__user_offers = []
 
     def process_sentence(self, sentence):
         # Tokenize the pattern
@@ -66,9 +68,6 @@ class Chatbot:
         # return tuple of intent and probability
         return return_list
 
-    def response(self):
-        pass
-
     def extract_amount(self, sentence):
         sentence = sentence.lower()
         # This regular expression will filter out amounts from the sentence like:
@@ -76,11 +75,49 @@ class Chatbot:
         amount = re.findall(r'rs\s?\.?([0-9]+\.?[0-9]+)', sentence)[0]
         return int(amount)
 
-    def track_user_offers(self, sentence):
-        pass
+    def generate_offer(self, min_price, original_price, user_price):
+        if (user_price > min_price) and (user_price <= original_price):
+            print('Done Deal')
+        elif user_price < min_price:
+            # If it's the first user offer and bot hasn't offered yet then
+            if len(self.__user_offers) == 1 and len(self.__bot_offers) == 0:
+                offer = self.compute_offer(min_price, user_price)
+            # If the bot has made any offers then compute the offer relative to the last offer
+            elif len(self.__bot_offers) > 0:
+                offer = self.compute_offer(min_price, self.__bot_offers[-1])
+            elif len(self.__user_offers) > 1:
+                offer = self.compute_offer(min_price, self.__user_offers[-1])
 
-    def track_bot_offers(self, sentence):
-        pass
+            return offer
 
-    def create_offer(self, min_price, original_price, bot_offer_count):
-        pass
+    def compute_offer_price(min_price, price):
+        # Compute an offer relative to previous offered price or user offer
+        offer = random.randint(min_price, price)
+        # Round the offer amount to the nearest 10
+        offer = offer - (offer % 10)
+        return offer
+
+    def response(self, sentence, user_id, show_details=False):
+        results = self.classify(sentence)
+        # if we have a classification then find the matching intent tag
+        if results:
+            # loop as long as there are matches to process
+            while results:
+                for i in self.intents['intents']:
+                    # find a tag matching the first result
+                    if i['tag'] == results[0][0]:
+                        # set context for this intent if necessary
+                        if 'context_set' in i:
+                            if show_details:
+                                print('context:', i['context_set'])
+                            self.context[user_id] = i['context_set']
+
+                        # check if this intent is contextual and applies to this user's conversation
+                        if not 'context_filter' in i or \
+                                (user_id in self.context and 'context_filter' in i and i['context_filter'] == self.context[user_id]):
+                            if show_details:
+                                print('tag:', i['tag'])
+                            # a random response from the intent
+                            return print(random.choice(i['responses']))
+
+                results.pop(0)
